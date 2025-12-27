98c5df0f08914cf364ac0f24cad005d87db99206
Paco Carrillo <pacocarrillom@gmail.com>
2025-12-28 00:37:28 +0100

    Configuración de ruta raíz para Hostinger

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto ayuda a que las rutas no se rompan
+  build: {
+    outDir: './', // <--- ¡ESTO ES LA CLAVE! Dice: "Pon los archivos en la raíz"
+  }
 })
